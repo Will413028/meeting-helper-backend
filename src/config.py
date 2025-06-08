97f52b0123d7be37c8d37d82ec52bd9b441e3575
Will <willwu@chengchitech.com>
2025-06-08 18:11:29 +0800

    ✏️ Fix typo

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 
 class Settings(BaseSettings):
     HUG_TOKEN: str = "REDACTED"
+    OUTPUT_DIR: str = os.getcwd()  # Default to current working directory
 
     model_config = SettingsConfigDict(env_file=".env")
 
