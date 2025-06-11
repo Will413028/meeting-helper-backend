4aca5b12fa86b13023b5d67dcae40c1d701d6ee7
Will <willwu@chengchitech.com>
2025-06-12 00:12:34 +0800

    ✨ Feat: add alembic

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 # Import our models and database configuration
-from src.database import Base, DATABASE_URL
+from src.models import Base, DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
