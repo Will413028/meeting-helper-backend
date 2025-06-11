1a5ab1dea942ecc6385a6dc680baaa64320e5d88
Will <willwu@chengchitech.com>
2025-06-12 00:13:05 +0800

    ✨ Feat: add user models

--- src/config.py
+++ src/config.py
@@ -5,8 +5,11 @@ import os
 class Settings(BaseSettings):
     HUG_TOKEN: str = "REDACTED"
     OUTPUT_DIR: str = os.getcwd()  # Default to current working directory
+    SECRET_KEY: str = "jindce9w5KEx4qBh9uN"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 43200
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="./env/.env")
 
 
 settings = Settings()
